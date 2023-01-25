df2b95b95b243faa8bebaae861af2ed6e5400601
DESKTOP-RCJOS5S\sangkook <sixstar6@naver.com>
2023-01-25 22:47:51 +0900

    서블릿 컨테이너 직접 생성후 기동

---
+++ src/main/java/com/sk/helloboot/App.java
@@ -0,0 +1,51 @@
+package com.sk.helloboot;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+    	WebServer webServer = serverFactory.getWebServer(servletContext ->{
+    		
+    		//프론트 컨트롤러 방식
+    		
+    		HelloController helloController = new HelloController();
+    		
+    		servletContext.addServlet("hello", new HttpServlet() {
+				private static final long serialVersionUID = -1527783874696319821L;
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp)
+						throws ServletException, IOException {
+					//요청URI주소에 따라 controller 매핑
+					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
+						
+						String ret = helloController.hello(name);
+						
+						//resp.setStatus(HttpStatus.OK.value()); 오류 없으면 서블릿컨테이너에서 200으로 자동 셋팅
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println(ret);
+					}else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
+				}
+			}).addMapping("/*");
+		});
+    	webServer.start();
+    }
+}
