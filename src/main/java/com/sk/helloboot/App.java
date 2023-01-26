833fa7f8c47f0f632c320eace99aa6ab8b87b93c
DESKTOP-RCJOS5S\sangkook <sixstar6@naver.com>
2023-01-27 06:42:59 +0900

    myapplication구성으로리팩토링

--- src/main/java/com/sk/helloboot/App.java
+++ src/main/java/com/sk/helloboot/App.java
@@ -2,6 +2,7 @@ package com.sk.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,24 +12,19 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class App {
+	
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+	
+	//ApplicationContextAware를 구현하면, applicationContext를 자동주입해준다.
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
 
     public static void main( String[] args ){
-    	//스프링 컨테이너 초기화 메서드 내에 서블릿컨테이너 생성 삽입
-    	AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-				TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		    	WebServer webServer = serverFactory.getWebServer(servletContext ->{
-		    		//DispatcherServlet으로 변경
-		    		servletContext.addServlet("hello", new DispatcherServlet(this)).addMapping("/*");
-				});
-		    	webServer.start();
-			}
-    	};
-    	
-    	//빈팩토리 매서드가 있는 java설정정보 등록
-    	applicationContext.register(App.class);
-    	applicationContext.refresh();//스프링 컨테이너 초기화
+    	MySpringApplication.run(App.class, args);
     }
 }
